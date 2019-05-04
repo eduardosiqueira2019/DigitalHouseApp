b1eaa5b43dfbbd8a7070f4b0cd2260132250682b
unknown <Digital@DH-A8-23.dhbrasil.digitalhouse.com>
2019-05-03 22:28:47 -0300

    Initial commit

---
+++ app/src/main/java/br/com/digitalhouse/app/MainActivity.java
@@ -0,0 +1,25 @@
+package br.com.digitalhouse.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editTextEmail;
+    EditText editTextSenha;
+    Button btnLogin;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editTextEmail = findViewById(R.id.edit_text_email);
+        editTextEmail = findViewById(R.id.edit_text_senha);
+        btnLogin = findViewById(R.id.btnlogin);
+    }
+}
