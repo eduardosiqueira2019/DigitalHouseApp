b5c76bf58efd09fbc604ded1638166e2a51679a7
eduardosiqueira2019 <siqueira007@yahoo.com.br>
2019-05-06 22:15:48 -0300

    commit

--- app/src/main/java/br/com/digitalhouse/app/MainActivity.java
+++ app/src/main/java/br/com/digitalhouse/app/MainActivity.java
@@ -2,8 +2,10 @@ package br.com.digitalhouse.app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,7 +21,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         editTextEmail = findViewById(R.id.edit_text_email);
-        editTextEmail = findViewById(R.id.edit_text_senha);
+        editTextSenha = findViewById(R.id.edit_text_senha);
         btnLogin = findViewById(R.id.btnlogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = editTextEmail.getText().toString();
+                String senha = editTextSenha.getText().toString();
+
+                if (email.equals("")){
+                    Toast.makeText(MainActivity.this, "Email não pode ser vazio", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if(senha.length() < 6){
+                    Toast.makeText(MainActivity.this, "Senha não pode ter menos de 6 caracteres.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (senha.equals("123456")){
+                    Toast.makeText(MainActivity.this, "Senha ok", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
